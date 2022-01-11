1238c88190a9639cccab82911acd27354bb04f2f
duvanjm <duvanjarin@gmail.com>
2022-01-11 18:11:55 -0500

    create Updateplayerscore and Getplayersleaderboard functions

--- backend/storage/db.go
+++ backend/storage/db.go
@@ -33,3 +33,27 @@ func AddPlayer(conn *pgx.Conn, p *models.PlayerModel) error {
 
 	return nil
 }
+
+// UpdatePlayerScore is used to update the player max score
+func UpdatePlayerScore(conn *pgx.Conn, p *models.PlayerModel) error {
+	query := "UPDATE players SET max_score = $1 WHERE player_id = $2 AND $1 > max_score;"
+
+	_, err := conn.Exec(context.Background(), query, p.MaxScore, p.PlayerID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetPlayersLeaderboard is used to return the top 10 best scores
+func GetPlayersLeaderboard(conn *pgx.Conn) (pgx.Rows, error) {
+	query := "SELECT username, max_score FROM players WHERE max_score > 0 ORDER BY max_score DESC LIMIT 10;"
+
+	rows, err := conn.Query(context.Background(), query)
+	if err != nil {
+		return nil, err
+	}
+
+	return rows, nil
+}
