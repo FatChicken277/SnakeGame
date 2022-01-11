15715d76c8226f2cf121fcad6abb31d86ac674ed
duvanjm <duvanjarin@gmail.com>
2022-01-11 18:01:32 -0500

    add function to add players to database

--- backend/storage/db.go
+++ backend/storage/db.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"context"
+	"SnakeGame/backend/models"
 
 	"github.com/jackc/pgx/v4"
 )
@@ -19,4 +20,16 @@ func DBConection(source string) (*pgx.Conn, error) {
 	}
 
 	return conn, nil
+}
+
+// AddPlayer is used to add a new player into database
+func AddPlayer(conn *pgx.Conn, p *models.PlayerModel) error {
+	query := "INSERT INTO players (username, password) VALUES ($1, $2);"
+
+	_, err := conn.Exec(context.Background(), query, p.Username, p.PasswordHash)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
