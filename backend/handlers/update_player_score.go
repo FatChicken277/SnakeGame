d09a7ad6b9913770f107f75d6cdfa1d5370596dc
duvanjm <duvanjarin@gmail.com>
2022-01-17 10:38:07 -0500

    Create UpdatePlayerScore function

--- backend/handlers/update_player_score.go
+++ backend/handlers/update_player_score.go
@@ -1,10 +1,11 @@
 package handlers
 
 import (
+	"SnakeGame/backend/models"
+	"SnakeGame/backend/storage"
 	"context"
 	"encoding/json"
 	"net/http"
-	"SnakeGame/backend/models"
 
 	"github.com/go-chi/jwtauth"
 	"github.com/jackc/pgx/v4"
@@ -13,7 +14,7 @@ import (
 func getAndVerifyUpdateScoreParams(dbConn *pgx.Conn, r *http.Request, player *models.PlayerModel) error {
 	_, claims, err := jwtauth.FromContext(r.Context())
 	if err != nil {
-			return err
+		return err
 	}
 
 	player.PlayerID = int(claims["player_id"].(float64)) + 1
@@ -22,17 +23,41 @@ func getAndVerifyUpdateScoreParams(dbConn *pgx.Conn, r *http.Request, player *mo
 	row := dbConn.QueryRow(context.Background(), query, player.PlayerID)
 	err = row.Scan(player.Username)
 	if err == pgx.ErrNoRows {
-			return ErrInvalidPlayer
+		return ErrInvalidPlayer
 	}
 
 	err = json.NewDecoder(r.Body).Decode(&player)
 	if err != nil {
-			return ErrInvalidJSON
+		return ErrInvalidJSON
 	}
 
 	if player.MaxScore == 0 {
-			return ErrMissingMaxScore
+		return ErrMissingMaxScore
 	}
 
 	return nil
+}
+
+// UpdatePlayerScore is used to update the score of a player
+func UpdatePlayerScore(dbConn *pgx.Conn) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var player models.PlayerModel
+
+		err := getAndVerifyUpdateScoreParams(dbConn, r, &player)
+		if err != nil {
+			LogError(NewErrorResponse(ErrorResponseBadRequest, w, err.Error()))
+			return
+		}
+
+		err = storage.UpdatePlayerScore(dbConn, &player)
+		if err != nil {
+			LogError(NewErrorResponse(ErrorResponseBadRequest, w, err.Error()))
+			return
+		}
+
+		resp := ResponseOK
+		resp.Message = "player score was successfuly updated"
+
+		LogError(NewResponse(resp, w))
+	}
 }
