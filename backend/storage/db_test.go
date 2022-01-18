eda90adb2d37cd4e0327d70da9819380ee64c40f
duvanjm <duvanjarin@gmail.com>
2022-01-18 12:38:27 -0500

    Create tests for database functions

--- backend/storage/db_test.go
+++ backend/storage/db_test.go
@@ -37,3 +37,38 @@ func TestAddPlayer(t *testing.T) {
 	_, err = dbConn.Exec(context.Background(), query)
 	c.NoError(err)
 }
+
+func TestUpdatePlayerScore(t *testing.T) {
+	c := require.New(t)
+
+	dbSource := "postgresql://admin@localhost:26257/snake_test?sslmode=disable"
+	dbConn, err := DBConection(dbSource)
+	c.NoError(err)
+	defer dbConn.Close(context.Background())
+
+	query := "INSERT INTO players (player_id, username, password) VALUES ($1, $2, $3);"
+	_, err = dbConn.Exec(context.Background(), query, "123", "example", "hash_password")
+	c.NoError(err)
+
+	examplePlayer := &models.PlayerModel{
+		PlayerID:     123,
+		Username:     "example",
+		PasswordHash: "hash_password",
+		MaxScore:     23,
+	}
+
+	err = UpdatePlayerScore(dbConn, examplePlayer)
+	c.NoError(err)
+
+	var score uint
+	query = "SELECT max_score FROM players WHERE player_id = 123;"
+	row := dbConn.QueryRow(context.Background(), query)
+	err = row.Scan(&score)
+	c.NoError(err)
+
+	c.Equal(score, examplePlayer.MaxScore)
+
+	query = "DELETE FROM players WHERE username = 'example';"
+	_, err = dbConn.Exec(context.Background(), query)
+	c.NoError(err)
+}
