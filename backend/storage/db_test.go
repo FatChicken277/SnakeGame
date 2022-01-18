93d9bc66b1285043ef52f6c538ac538e8218312c
duvanjm <duvanjarin@gmail.com>
2022-01-18 12:31:23 -0500

    Create tests for database functions

--- backend/storage/db_test.go
+++ backend/storage/db_test.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"SnakeGame/backend/models"
 	"context"
 	"testing"
 
@@ -14,4 +15,25 @@ func TestDBConection(t *testing.T) {
 	dbConn, err := DBConection(dbSource)
 	c.NoError(err)
 	dbConn.Close(context.Background())
+}
+
+func TestAddPlayer(t *testing.T) {
+	c := require.New(t)
+
+	dbSource := "postgresql://admin@localhost:26257/snake_test?sslmode=disable"
+	dbConn, err := DBConection(dbSource)
+	c.NoError(err)
+	defer dbConn.Close(context.Background())
+
+	newPlayer := &models.PlayerModel{
+		Username:     "example",
+		PasswordHash: "hash_password",
+	}
+
+	err = AddPlayer(dbConn, newPlayer)
+	c.NoError(err)
+
+	query := "DELETE FROM players WHERE username = 'example';"
+	_, err = dbConn.Exec(context.Background(), query)
+	c.NoError(err)
 }
