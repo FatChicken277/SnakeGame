b0eda9ebd739e59230609ceec11672e6b359f063
duvanjm <duvanjarin@gmail.com>
2022-01-24 13:36:53 -0500

    Create Home view

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <WelcomeImage/>
+  </div>
+</template>
+
+<script>
+import WelcomeImage from '@/components/Home/WelcomeImage.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    WelcomeImage,
+  },
+};
+</script>
