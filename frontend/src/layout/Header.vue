76d3229cc4c7bf2039b0d0e472bf2c8a6370cd24
FatChicken277 <aramirez@truora.com>
2022-01-24 14:55:00 -0500

    add layouts and add it into app.vue and router config

---
+++ frontend/src/layout/Header.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-app-bar
+      app
+      color="secondary"
+      dark
+      class="px-9"
+  >
+    <div class="d-flex align-center">
+      <v-img
+          alt="Snake Logo"
+          class="shrink mr-2"
+          src="../assets/logo.png"
+          transition="scale-transition"
+          width="50"
+      />
+      <h1 style="text-shadow: 4px 4px black;">Snakee</h1>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <router-link to="/register" v-if="!loggedIn">
+      <v-btn
+        color="primary"
+        outlined
+        class="mr-4"
+      >
+        Register
+      </v-btn>
+    </router-link>
+    <router-link to="/login" v-if="!loggedIn">
+      <v-btn
+        color="primary"
+        style="color: #1E1E1E;"
+      >
+        Login
+      </v-btn>
+    </router-link>
+    <v-btn
+      v-if="loggedIn"
+      color="primary"
+      outlined
+      @click="signOut"
+    >
+      Logout
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  computed: {
+    loggedIn() {
+      return this.$store.getters.loggedIn;
+    },
+  },
+  methods: {
+    async signOut() {
+      await this.$store.dispatch('signOut');
+      this.$router.push('/');
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="home">
-    <WelcomeImage/>
   </div>
 </template>
 
 <script>
-import WelcomeImage from '@/components/Home/WelcomeImage.vue';
 
 export default {
   name: 'Home',
   components: {
-    WelcomeImage,
   },
 };
 </script>
