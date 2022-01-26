405eb8c5f48c346f413c4978b43ad3f772407ebf
FatChicken277 <1637@holbertonschool.com>
2022-01-25 21:56:08 -0500

    Add leaderboard component and add access control to views

--- frontend/src/views/Game.vue
+++ frontend/src/views/Game.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="game">
     <v-container class="fill-height">
-      <v-row aling="center">
-        <v-col cols="8">
+      <v-row justify="center" style="margin-top: 15vh;">
+        <v-col cols="8" xs="12" sm="12" md="12" lg="8">
           <v-sheet
             class="game-sheet px-5"
             rounded
@@ -12,6 +12,10 @@
             <GameComponent />
           </v-sheet>
         </v-col>
+        <v-spacer />
+        <v-col cols="3" xs="10" sm="10" md="10" lg="3">
+          <LeaderboardComponent />
+        </v-col>
       </v-row>
     </v-container>
   </div>
@@ -19,17 +23,19 @@
 
 <script>
 import GameComponent from '@/components/Game/GameComponent.vue';
+import LeaderboardComponent from '@/components/Game/LeaderboardComponent.vue';
 
 export default {
   name: 'Game',
   components: {
     GameComponent,
+    LeaderboardComponent,
   },
 };
 </script>
 
 <style>
 .game {
-  height: 100vh;
+  min-height: 100vh;
 }
 </style>
